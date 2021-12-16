fc06c0823e381676d5f63b1a51dfd79754a3735d
liulei <15019474951@163.com>
2021-12-16 22:50:44 +0800

    分页查询插件

--- src/main/java/top/xiaorang/mybatisplusstudy/config/MybatisPlusConfig.java
+++ src/main/java/top/xiaorang/mybatisplusstudy/config/MybatisPlusConfig.java
@@ -2,6 +2,7 @@ package top.xiaorang.mybatisplusstudy.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,7 @@ public class MybatisPlusConfig {
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
         interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
         return interceptor;
     }
 }
--- src/test/java/top/xiaorang/mybatisplusstudy/SampleTest.java
+++ src/test/java/top/xiaorang/mybatisplusstudy/SampleTest.java
@@ -1,55 +1,84 @@
 package top.xiaorang.mybatisplusstudy;
 
-import com.baomidou.mybatisplus.core.toolkit.Assert;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.xiaorang.mybatisplusstudy.entity.User;
 import top.xiaorang.mybatisplusstudy.mapper.UserMapper;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 @SpringBootTest
 public class SampleTest {
-    @Autowired
-    private UserMapper userMapper;
+  @Autowired private UserMapper userMapper;
 
-    @Test
-    public void testSelect() {
-        System.out.println(("----- selectAll method test ------"));
-        List<User> userList = userMapper.selectList(null);
-        userList.forEach(System.out::println);
-    }
+  @Test
+  public void testSelect() {
+    System.out.println(("----- selectAll method test ------"));
+    List<User> userList = userMapper.selectList(null);
+    userList.forEach(System.out::println);
+  }
 
-    @Test
-    public void testInsert() {
-        User user = User.builder().name("小让的糖果屋4").age(21).email("2329862718@qq.com").build();
-        userMapper.insert(user);
-        System.out.println(user);
-    }
+  @Test
+  public void testInsert() {
+    User user = User.builder().name("小让的糖果屋4").age(21).email("2329862718@qq.com").build();
+    userMapper.insert(user);
+    System.out.println(user);
+  }
 
-    @Test
-    public void testUpdate() {
-        User user = User.builder().id(1471083433073393675L).age(30).email("15019474951@163.com").build();
-        userMapper.updateById(user);
-    }
+  @Test
+  public void testUpdate() {
+    User user =
+        User.builder().id(1471083433073393675L).age(30).email("15019474951@163.com").build();
+    userMapper.updateById(user);
+  }
 
-    @Test
-    public void testUpdate2() {
-        User user = userMapper.selectById(1471083433073393675L);
-        user.setAge(30);
-        userMapper.updateById(user);
-    }
+  @Test
+  public void testUpdate2() {
+    User user = userMapper.selectById(1471083433073393675L);
+    user.setAge(30);
+    userMapper.updateById(user);
+  }
 
-    @Test
-    public void testUpdate3() {
-        User user = userMapper.selectById(1471083433073393675L);
+  @Test
+  public void testUpdate3() {
+    User user = userMapper.selectById(1471083433073393675L);
 
-        User user2 = userMapper.selectById(1471083433073393675L);
-        user2.setAge(32);
-        userMapper.updateById(user2);
+    User user2 = userMapper.selectById(1471083433073393675L);
+    user2.setAge(32);
+    userMapper.updateById(user2);
 
-        user.setAge(30);
-        userMapper.updateById(user);
-    }
+    user.setAge(30);
+    userMapper.updateById(user);
+  }
+
+  @Test
+  public void testSelectByBatchId() {
+    List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+    users.forEach(System.out::println);
+  }
+
+  @Test
+  public void testSelectByMap() {
+    HashMap<String, Object> map = new HashMap<>();
+    map.put("name", "Jone");
+    map.put("age", 18);
+    List<User> users = userMapper.selectByMap(map);
+    users.forEach(System.out::println);
+  }
+
+  // 测试分页查询
+  @Test
+  public void testPage() {
+    // 参数一：当前页
+    // 参数二：页面大小
+    // 使用了分页插件之后，所有的分页操作也变得简单的！
+    Page<User> page = new Page<>(2, 5);
+    userMapper.selectPage(page, null);
+    page.getRecords().forEach(System.out::println);
+    System.out.println(page.getTotal());
+  }
 }
