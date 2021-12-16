60df548dab175e847d1b7fe852913cec7a527c7e
liulei <15019474951@163.com>
2021-12-16 17:05:44 +0800

    自动填充功能（代码级别）

--- src/main/java/top/xiaorang/mybatisplusstudy/entity/User.java
+++ src/main/java/top/xiaorang/mybatisplusstudy/entity/User.java
@@ -1,6 +1,8 @@
 package top.xiaorang.mybatisplusstudy.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +17,8 @@ public class User {
     private String name;
     private Integer age;
     private String email;
+    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 }
